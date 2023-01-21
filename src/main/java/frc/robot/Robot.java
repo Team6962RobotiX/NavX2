72ba77f58ce1663dfbd26cb5b7331240a8b50277
Arjun Bhobe <arjun@bhobe.com>
2023-01-21 15:19:27 -0800

    Add the simulator functions

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,7 +12,8 @@ import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * This is a sample program to demonstrate how to use a gyro sensor to make a robot drive straight.
@@ -34,11 +35,12 @@ public class Robot extends TimedRobot {
   private final Joystick m_joystick = new Joystick(kJoystickPort);
   private final AHRS m_gyro = new AHRS(I2C.Port.kMXP);
 
+  private final long initTime = System.currentTimeMillis();
+
 
   @Override
   public void robotInit() {
     m_gyro.calibrate();
-    m_gyro.getPitch();
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
@@ -66,4 +68,43 @@ public class Robot extends TimedRobot {
       st2 = true;
     }
   }
+
+  public void teleopPeriodic() {
+
+    long currentTime = initTime - System.currentTimeMillis();
+
+    
+    boolean st1 = false;
+    boolean st2 = false;
+    double angleRatio = m_gyro.getPitch()/180;
+
+    
+    while (!st1) {
+      m_myRobot.tankDrive(1.0, 1.0);
+      if (getLyse((int)(currentTime/100)) > 25) {
+        st1 = true;
+      }
+    } while (!st2) {
+      m_myRobot.tankDrive(angleRatio, angleRatio);
+      st2 = true;
+    }
+  }
+
+
+
+
+
+  private static Random rand = new Random();   
+  public static Float getLyse(int time) {
+    ArrayList<Float> values = new ArrayList<Float>();
+    for(int i = 0; i < 100; i++){
+      values.add(randValue());
+    }
+		return values.get(time);
+  }
+
+  public static float randValue(){
+    return rand.nextFloat()*68 - 34;
+  }
+
 }
